b659c927afc760afec0a6d24e046df714721d25b
razgraf <razvan.gabriel.apostu@gmail.com>
2020-03-31 14:16:01 +0300

    l7: data members and wcf interface

---
+++ Homework/P2/MyPhotos/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MyPhotos.Model;
+using MyPhotos.Storage;
+
+namespace MyPhotos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Start");
+            DbService dbService = new DbService();
+        /*   dbService.InitDb();*/
+
+            List<File> files = dbService.GetFiles();
+            foreach (File f in files)
+                Console.WriteLine(f.ToString());
+
+            Console.WriteLine("End");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Homework/P2/MyPhotosWCF/InterfaceWCF.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+using MyPhotos;
+
+namespace MyPhotosWCF
+{
+    [ServiceContract]
+    interface InterfaceWCF
+    {
+
+        [OperationContract]
+        void UpdateFileDataByFileIdAndLabel(Guid fileId, string label, string value);
+
+        [OperationContract]
+        MyPhotos.Model.File CreateFile(string name, string path);
+
+        [OperationContract]
+        MyPhotos.Model.Data CreateData(string label, bool isSearchable = true);
+
+        [OperationContract]
+        MyPhotos.Model.FileData CreateFileData(MyPhotos.Model.File file, MyPhotos.Model.Data data, string value);
+
+        [OperationContract]
+        void RemoveFileById(Guid id);
+
+        [OperationContract]
+        void RemoveDataById(Guid id);
+
+        [OperationContract]
+        MyPhotos.Model.File GetFileById(Guid id);
+
+        [OperationContract]
+        MyPhotos.Model.Data GetDataById(Guid id);
+
+        [OperationContract]
+        List<MyPhotos.Model.File> GetFiles();
+
+        [OperationContract]
+        List<MyPhotos.Model.Data> GetDatas();
+
+        [OperationContract]
+        List<MyPhotos.Model.FileData> GetFileDatas();
+
+        [OperationContract]
+        List<MyPhotos.Model.FileData> GetFileDatasByFileId(Guid fileId);
+
+        [OperationContract]
+        List<MyPhotos.Model.File> GetFilteredFiles(Func<MyPhotos.Model.File, bool> filter);
+
+        [OperationContract]
+
+        List<MyPhotos.Model.Data> GetFilteredDatas(Func<MyPhotos.Model.Data, bool> filter);
+
+        [OperationContract]
+        List<MyPhotos.Model.FileData> GetFilteredFileDatas(Func<MyPhotos.Model.FileData, bool> filter);
+
+    }
+}
