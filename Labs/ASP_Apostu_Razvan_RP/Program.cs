4a7c4ac291bc7d9782c48eb53e4e7d3c9448c425
razgraf <razvan.gabriel.apostu@gmail.com>
2020-05-05 14:55:01 +0300

    l10: db seed

--- Labs/ASP_Apostu_Razvan_RP/Program.cs
+++ Labs/ASP_Apostu_Razvan_RP/Program.cs
@@ -4,8 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+/** https://github.com/dotnet/aspnetcore/issues/2213 */
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+
+using ASP_Apostu_Razvan_RP.Data;
+using ASP_Apostu_Razvan_RP.Models;
 
 namespace ASP_Apostu_Razvan_RP
 {
@@ -13,7 +18,24 @@ namespace ASP_Apostu_Razvan_RP
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    SeedData.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
