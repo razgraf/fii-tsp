07f9dd81115a23a98fb8ed1a16ce2f843cb2768c
razgraf <razvan.gabriel.apostu@gmail.com>
2020-05-12 14:08:19 +0300

    l11: this one will not be completed. thank you.

---
+++ Labs/EFWCFASPL11/Client/Program.cs
@@ -0,0 +1,32 @@
+﻿using Client.ServiceReferencePC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Client
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            /*Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+*/
+
+            ServiceReferencePC.PostCommentClient client = new ServiceReferencePC.PostCommentClient();
+            List<Post> posts = client.GetPosts().ToList();
+
+            foreach (Post p in posts)
+                Console.WriteLine(p.ToString());
+
+        }
+    }
+}
