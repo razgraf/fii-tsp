3e3f212563f28e54e7e4cb2b5d018d765af0aee5
razgraf <razvan.gabriel.apostu@gmail.com>
2020-05-12 14:56:23 +0300

    l11: remove post + list comment

---
+++ Labs/EFWCFASPL11/WebAppPostComment/Pages/Comments/List.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppPostComment.Models;
+using ServiceReferencePostComment;
+
+
+namespace WebAppPostComment.Pages.Comments
+{
+    public class ListModel : PageModel
+    {
+        PostCommentClient pcc = new PostCommentClient();
+        public List<CommentDTO> Comments { get; set; }
+        public ListModel() { Comments = new List<CommentDTO>(); }
+        public async Task OnGetAsync(int? id)
+        { 
+            if (!id.HasValue) return; 
+            var item = await pcc.GetPostByIdAsync(id.Value);
+            ViewData["Post"] = item.PostId.ToString() + " : " + item.Description.Trim();
+            foreach (var cc in item.Comments) 
+            {
+                CommentDTO cdto = new CommentDTO();
+                cdto.PostPostId = cc.PostPostId;
+                cdto.Text = cc.Text; 
+                cdto.CommentId = cc.CommentId;
+                Comments.Add(cdto);
+            } 
+        }
+    }
+}
